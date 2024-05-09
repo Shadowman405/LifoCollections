2edd38b7391bf2b36a5619c113b3445744aa683d
Shadowman405 <maximus405@gmail.com>
2024-05-09 21:06:07 +0300

    Added initial framework project

---
+++ LifoCollections/LifoCollections/LifoCollections.h
@@ -0,0 +1,18 @@
+//
+//  LifoCollections.h
+//  LifoCollections
+//
+//  Created by Maxim Mitin on 9.05.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LifoCollections.
+FOUNDATION_EXPORT double LifoCollectionsVersionNumber;
+
+//! Project version string for LifoCollections.
+FOUNDATION_EXPORT const unsigned char LifoCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifoCollections/PublicHeader.h>
+
+
