efe942fd7b77766d79657e5a56879e84e21a50e4
Shadowman405 <maximus405@gmail.com>
2024-05-11 21:26:34 +0300

    Updated bundle ID, updated current versions properties, removed unnecesseary comments in file

--- LifoCollections/LifoCollections/LifoCollections.h
+++ LifoCollections/LifoCollections/LifoCollections.h
@@ -1,10 +1,3 @@
-//
-//  LifoCollections.h
-//  LifoCollections
-//
-//  Created by Maxim Mitin on 9.05.24.
-//
-
 #import <Foundation/Foundation.h>
 
 //! Project version number for LifoCollections.
@@ -12,7 +5,3 @@ FOUNDATION_EXPORT double LifoCollectionsVersionNumber;
 
 //! Project version string for LifoCollections.
 FOUNDATION_EXPORT const unsigned char LifoCollectionsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <LifoCollections/PublicHeader.h>
-
-
