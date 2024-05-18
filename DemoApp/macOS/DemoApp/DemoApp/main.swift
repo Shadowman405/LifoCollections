68700555de2289b4570067720f2b1d0ffc4cab87
Shadowman405 <maximus405@gmail.com>
2024-05-18 19:39:34 +0300

    Updated formating to main.swift using swiftformat

--- DemoApp/macOS/DemoApp/DemoApp/main.swift
+++ DemoApp/macOS/DemoApp/DemoApp/main.swift
@@ -4,6 +4,6 @@ let someElements = [1, 2, 3, 4, 5]
 let stack = StackStatistics(someElements)
 
 print("Elements in stack: \(stack.count)")
-if let minimumElement = stack.minimumElement(){
+if let minimumElement = stack.minimumElement() {
     print("Minimum element in stak is: \(minimumElement)")
 }
