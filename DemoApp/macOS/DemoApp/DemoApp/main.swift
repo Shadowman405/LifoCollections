5f18253904c51bb97f6ad49fb9207f823a491e48
Shadowman405 <maximus405@gmail.com>
2024-05-19 22:22:09 +0300

    Fixed typos in print text

--- DemoApp/macOS/DemoApp/DemoApp/main.swift
+++ DemoApp/macOS/DemoApp/DemoApp/main.swift
@@ -3,7 +3,7 @@ import LifoCollections
 let someElements = [1, 2, 3, 4, 5]
 let stack = StackStatistics(someElements)
 
-print("Elements in stack: \(stack.count)")
+print("Elements in the stack: \(stack.count)")
 if let minimumElement = stack.minimumElement() {
-    print("Minimum element in stak is: \(minimumElement)")
+    print("Minimum element in the stack is: \(minimumElement)")
 }
